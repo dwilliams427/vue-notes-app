8e3206ff04ba4c7786790b6e24778ba7ba160137
Daniel Williams <dlw1ll27@gmail.com>
2021-04-05 11:55:06 -0700

    initial commit

---
+++ src/components/NoteContainer.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="note-container">
+    <NoteSelectors />
+    <!-- <div class="note-selectors">
+      <div class="note-selector active">
+        <p class="note-selector-title">First note...</p>
+        <p class="note-selector-timestamp">Timestamp here...</p>
+      </div>
+      <div class="note-selector">
+        <p class="note-selector-title">Second note...</p>
+        <p class="note-selector-timestamp">Timestamp here...</p>
+      </div>
+      <div class="note-selector">
+        <p class="note-selector-title">Third note...</p>
+        <p class="note-selector-timestamp">Timestamp here...</p>
+      </div>
+    </div> -->
+    <NoteEditor />
+    <!-- <div class="note-editor">
+      <p class="note-editor-info">Timestamp here...</p>
+      <textarea class="note-editor-input">
+          First note...
+          Note text here...
+        </textarea
+      >
+    </div> -->
+  </div>
+</template>
+<script>
+import NoteSelectors from "./NoteSelectors";
+import NoteEditor from "./NoteEditor";
+
+export default {
+  components: {
+    NoteSelectors,
+    NoteEditor,
+  },
+};
+</script>
---
+++ src/components/NoteSelector.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="root">
+    <div class="note-selector">
+      <p class="note-selector-title">{{ note.body }}</p>
+      <p class="note-selector-timestamp">{{ note.timestamp }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["note"],
+};
+</script>
